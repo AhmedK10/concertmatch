152b7367aec6d1204e0022fe220c142e2b16cb0a
Jasmeet Samra <js.samra@outlook.com>
2023-06-06 16:23:23 +0200

    kithen/sink root created, colors and font created for the front end

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   # Defines the root path route ("/")
   # root "articles#index"
-  
-  
+  get "/kitchen_sink", to: "pages#kitchen_sink"
+
   resources :concerts, only: [:index, :show] do
     resources :forums, only: [:index, :create]
     resources :favorites, only: [:create]
