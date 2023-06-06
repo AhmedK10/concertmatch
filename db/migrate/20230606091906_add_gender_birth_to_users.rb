1ddb6be04a366604b63708e6b6054980360097aa
Ahmed Khogali <ahmed.khog@outlook.com>
2023-06-06 11:19:44 +0200

    genrated all models

---
+++ db/migrate/20230606091906_add_gender_birth_to_users.rb
@@ -0,0 +1,6 @@
+class AddGenderBirthToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :gender, :integer
+    add_column :users, :date_of_birth, :date
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_091610) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_091906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,6 +79,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_091610) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.integer "gender"
+    t.date "date_of_birth"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
