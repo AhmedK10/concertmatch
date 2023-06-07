6b524ff7ed292a10721e874484736eb2936ad417
onur deryahanoglu <onur.deryahan@gmail.com>
2023-06-07 10:51:19 +0200

    associations updated

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -3,11 +3,8 @@ class Concert < ApplicationRecord
     validates :name, presence: true
     validates :date, presence: true
     validates :address, presence:true
-    
-    has_many :performances
+ 
     has_many :forums
     has_many :favorites
-    has_many :users, through: :favorites
-    has_many :comments, through: :forums
-    
+    has_many :users, through: :favorites 
 end
