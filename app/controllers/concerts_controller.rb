4b269119e85fcaed385d37cf977be969c1fb10e6
Ahmed Khogali <ahmed.khog@outlook.com>
2023-06-07 12:45:18 +0200

    added serching by genre

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -4,6 +4,8 @@ class ConcertsController < ApplicationController
   def index
     @concerts = Concert.all
     if params[:query].present?
+      sql_subquery = "genre ILIKE :query OR summary ILIKE :query"
+
       @concerts = @concerts.where("genre ILIKE ?", "%#{params[:query]}%")
     end
   end
