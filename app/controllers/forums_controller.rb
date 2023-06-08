47f09dbab52cfd43ceb0b08ffbc80039ee55eddc
Ahmed Khogali <ahmed.khog@outlook.com>
2023-06-08 15:29:38 +0200

    finished board filter

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -2,6 +2,20 @@ class ForumsController < ApplicationController
   def index
     @concert = Concert.find(params[:concert_id])
     @forums = @concert.forums
+    
+    if params[:query].present?
+      @forums = @forums.where("board ILIKE ?", "%#{params[:query]}%")
+    end
+
+    board_type = params[:board_type]
+
+    if board_type == "accommodation"
+      @forums = @forums.where(board: :accommodation)
+    elsif board_type == "travel"
+      @forums = @forums.where(board: :travel)
+    else
+      @forums = @forums.where(board: :buddies)
+    end
   end
 
   def show
