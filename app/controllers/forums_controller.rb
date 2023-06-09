f6beac6c7b3b80da380045238ca82b67aad6776f
Jasmeet Samra <js.samra@outlook.com>
2023-06-09 12:16:53 +0200

    forums filter adjusted and improved

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -4,57 +4,34 @@ class ForumsController < ApplicationController
     @forums = @concert.forums.order(created_at: :desc)
     @forum = Forum.new
 
-    #forum type filter
     if params[:query].present?
       @forums = @forums.where("board ILIKE ?", "%#{params[:query]}%")
     end
 
-    board_type = params[:board_type]
-    if board_type == "accommodation"
-      @forums = @forums.where(board: :accommodation)
-    elsif board_type == "travel"
-      @forums = @forums.where(board: :travel)
-    else
-      @forums = @forums.where(board: :buddies)
+    if params[:board_type].present?
+      @forums = @forums.where(board: params[:board_type])
     end
 
-    #User filter
-    @users = User.all
-
-    #gender filter
-    if params[:query].present?
-      @users = @users.where("gender ILIKE ?", "%#{params[:query]}%")
-    end
-    gender = params[:gender]
-    if gender == "male"
-      @forums = @forums.select { |forum| forum.user.gender == "male"}
-    elsif gender == "female"
-      @forums = @forums.select { |forum| forum.user.gender == "female"}
-    else
-      @forums = @forums.select { |forum| forum.user.gender == "other"}
+    if params[:gender].present?
+      @forums = @forums.joins(:user).where(users: { gender: params[:gender] })
     end
 
-    #age filter
-    if params[:query].present?
-      @users = @users.where("age ILIKE ?", "%#{params[:query]}%")
+    if params[:age_range].present?
+      case params[:age_range]
+      when "18-25"
+        @forums = @forums.joins(:user).where("users.date_of_birth >= ? AND users.date_of_birth <= ?", 25.years.ago, 18.years.ago)
+      when "26-35"
+        @forums = @forums.joins(:user).where("users.date_of_birth >= ? AND users.date_of_birth <= ?", 35.years.ago, 26.years.ago)
+      when "36-45"
+        @forums = @forums.joins(:user).where("users.date_of_birth >= ? AND users.date_of_birth <= ?", 45.years.ago, 36.years.ago)
+      when "46-60"
+        @forums = @forums.joins(:user).where("users.date_of_birth >= ? AND users.date_of_birth <= ?", 60.years.ago, 46.years.ago)
+      when "60+"
+        @forums = @forums.joins(:user).where("users.date_of_birth <= ?", 60.years.ago)
+      end
     end
-
-    age = params[:age]
-    if age.to_i > 18 && age.to_i < 25
-      @forums = @forums.select { |forum| forum.user.age >= 18 && forum.user.age <= 25 }
-    elsif age
-      @forums = @forums.select { |forum| forum.user.age >= 25 && forum.user.age <= 35 }
-    elsif age
-      @forums = @forums.select { |forum| forum.user.age >= 35 && forum.user.age <= 45 }
-    elsif age
-      @forums = @forums.select { |forum| forum.user.age >= 45 && forum.user.age <= 55 }
-    elsif age
-      @forums = @forums.select { |forum| forum.user.age >= 55 && forum.user.age <= 65 }
-    elsif age
-      @forums = @forums.select { |forum| forum.user.age >= 65 && forum.user.age <= 75 }
-    end
-
   end
+
 
   def show
     @forum = Forum.find(params[:id])
