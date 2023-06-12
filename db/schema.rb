1e253074d5a391e04d8c4ba428e46356bb12569a
Ahmed Khogali <ahmed.khog@outlook.com>
2023-06-12 11:00:11 +0200

    basic chatroom implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_09_094152) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_075623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_09_094152) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
@@ -83,6 +89,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_09_094152) do
     t.integer "board"
     t.index ["concert_id"], name: "index_forums_on_concert_id"
     t.index ["user_id"], name: "index_forums_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "chatroom_id"
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "taggings", force: :cascade do |t|
@@ -140,5 +156,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_09_094152) do
   add_foreign_key "favorites", "users"
   add_foreign_key "forums", "concerts"
   add_foreign_key "forums", "users"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "taggings", "tags"
 end
