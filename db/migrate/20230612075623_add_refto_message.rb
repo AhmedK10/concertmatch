53c3357d99def2fbe2cc7b99e08d5b3ed2a65f49
Ahmed Khogali <ahmed.khog@outlook.com>
2023-06-12 10:04:42 +0200

    about to run addref migration for second try

--- db/migrate/20230612075623_add_refto_message.rb
+++ db/migrate/20230612075623_add_refto_message.rb
@@ -1,9 +1,6 @@
 class AddReftoMessage < ActiveRecord::Migration[7.0]
   def change
-    def change
-      create_table :messages do |t|
-        t.references :chatroom, foreign_key: true
-        t.references :user, foreign_key: true
-      end
+    add_reference :messages, :user, foreign_key: true
+    add_reference :messages, :chatroom, foreign_key: true
   end
 end
