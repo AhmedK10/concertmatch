43368a338fa7215b56c5938ab81d043d7c78435b
Ahmed Khogali <ahmed.khog@outlook.com>
2023-06-13 17:27:04 +0200

    linked private chats to concerts#show

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,9 @@ Rails.application.routes.draw do
   resources :favorites, only: :destroy
   resources :comments, only: :destroy
 
-  resources :chatrooms, only: [:show, :create] do
+  resources :chatrooms, only: [:show] do
     resources :messages, only: :create
   end
+
+  post ':user_id/chatrooms', to: 'chatrooms#create', as: :chatrooms
 end
