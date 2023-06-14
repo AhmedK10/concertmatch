0b32e87dc7ba5c6eb07bf73f1cdeb8dbb0ea4f08
Ahmed Khogali <ahmed.khog@outlook.com>
2023-06-14 14:38:24 +0200

    fixed time in chat & added city, country

--- db/migrate/20230614121533_addcitytoconcert.rb
+++ db/migrate/20230614121533_addcitytoconcert.rb
@@ -1,4 +1,4 @@
-class AddCityandCountrytoConcert < ActiveRecord::Migration[7.0]
+class Addcitytoconcert < ActiveRecord::Migration[7.0]
   def change
     add_column :concerts, :city, :string
     add_column :concerts, :country, :string
