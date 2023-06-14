2accc9e92965be6c6204234ac924caea37c29974
Yuta Asazuma <asa14151415@gmail.com>
2023-06-14 12:53:40 +0200

    Modified profile page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
 class UserController < ApplicationController
   def index
-    
-    
+    @favorite = Favorite.all
+    @favorite = Favorite.find_by(user: current_user, concert: @concert)
   end
 end
